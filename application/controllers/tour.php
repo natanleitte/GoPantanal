6228dc933fafb010f4a30a32494c3ac6a1e02954
natanleitte <natanleitte@gmail.com>
2016-04-22 16:54:06 -0300

    hotel_tour

--- application/controllers/tour.php
+++ application/controllers/tour.php
@@ -10,6 +10,7 @@ class Tour extends CI_Controller {
         $this->load->model('EmailModel');
         $this->load->model('TourModel');
         $this->load->model('ClienteModel');
+        $this->load->model('HotelModel');
     }
 
     public function index() {
@@ -29,18 +30,13 @@ class Tour extends CI_Controller {
 
     public function tour() {
         $this->configuracoesBasicasParaCarregarPagina();
-
         $data['tarefas'] = $this->TarefaModel->getTarefas();
         $data['emails'] = $this->EmailModel->obterTodos();
         $data['qtdDeEmailsNaoLidos'] = $this->EmailModel->obterQuantidadeDeEmailsNaoLidos();
         $data['ultimosCincoEmails'] = $this->EmailModel->obterOsUltimosCincoEmails();
         $data['cliente'] = $this->ClienteModel->getCliente($this->input->get('id', TRUE));
-
-
-//        $data['tour'] = $this->TourModel->getTour($this->input->get('id'));
-//        echo $data['tour']->id_cliente;
-//        $data['cliente'] = $this->ClienteModel->getCliente($data['tour']->id_cliente);
-
+        $data['hoteis'] = $this->HotelModel->getHoteis();
+        
         $this->load->view('header', $data);
         $this->load->view('tour/tour', $data);
         $this->load->view('footer');
--- application/views/tour/tour.php
+++ application/views/tour/tour.php
@@ -3,6 +3,15 @@ foreach ($cliente->result() as $row) {
     $cliente = $row;
 }
 ?>
+
+<script type="text/javascript">
+    $(function () {
+        $('#datetimepicker_ini, #datetimepicker_fim').datetimepicker({
+            format: 'DD/MM/YYYY'
+        });
+    });
+</script>
+
 <section id="content">
     <div class="container">
 
@@ -45,47 +54,70 @@ foreach ($cliente->result() as $row) {
                     <div class="pmb-block">
                         <div class="pmbb-header">
                             <h2><i class="zmdi zmdi-assignment-o m-r-5"></i> Agende os Hotéis</h2>
-                            <div class="listview lv-bordered lv-lg">
-
-                                <div class="lv-body">
-                                    <div class="lv-item media">
-                                        <div class="checkbox pull-left">
-                                            <label>
-                                                <input type="checkbox" value="">
-                                                <i class="input-helper"></i>
-                                            </label>
-                                        </div>
-                                        <div class="media-body">
-                                            <div class="lv-title">Per an error perpetua, fierent fastidii recteque ad pro. Mei id brute intellegam</div>
-                                        </div>
-                                    </div>
-
-                                    <div class="lv-item media">
-                                        <div class="checkbox pull-left">
-                                            <label>
-                                                <input type="checkbox" value="">
-                                                <i class="input-helper"></i>
-                                            </label>
-                                        </div>
-                                        <div class="media-body">
-                                            <div class="lv-title">Per an error perpetua, fierent fastidii recteque ad pro. Mei id brute intellegam</div>
+                            <div class="col-sm-6">
+                                <div class="col-sm-6">
+                                    <div class="form-group">
+                                        <div class="fg-line">
+                                            <div class="select">
+                                                <select name="origem_contato" class="form-control" placeholder="Origem do Contato">
+                                                    <?php
+                                                    foreach ($hoteis->result() as $hotel) {
+                                                        echo "<option value = ''>" . $hotel->nome . "</option>";
+                                                    }
+                                                    ?>
+                                                    <option value="">Origem do contato...</option>
+                                                    <option value="Email">Email</option>
+                                                    <option value="Telefone">Telefone</option>
+                                                    <option value="Site">Site</option>
+                                                    <option value="Pessoal">Pessoal</option>
+                                                </select>
+                                            </div>
                                         </div>
                                     </div>
-                                </div>                        
+                                </div>
+
+                                <div class="col-sm-4">
+                                    <div class='input-group date' id='datetimepicker_ini'>
+                                        <input type='text' class="form-control" name="data_contato" placeholder="Entrada" />
+                                        <span class="input-group-addon">
+                                            <span class="glyphicon glyphicon-calendar"></span>
+                                        </span>
+                                    </div>
+                                </div>
+
+                                <div class="col-sm-4">
+                                    <div class='input-group date' id='datetimepicker_fim'>
+                                        <input type='text' class="form-control" name="data_contato" placeholder="Saída" />
+                                        <span class="input-group-addon">
+                                            <span class="glyphicon glyphicon-calendar"></span>
+                                        </span>
+                                    </div>
+                                </div>
+                                <button onclick="adicionarHotelTour()" class="btn bgm-blue btn-default btn-icon-text"><i class="zmdi zmdi-arrow-forward"></i> Adicionar Hotel</button>
+
                             </div>
-                        </div>
-
-                        <div class="pmb-block">
-                            <div class="pmbb-header">
-                                <h2><i class="zmdi zmdi-account m-r-5"></i> Informações</h2>
-                                <ul class="actions">
-                                    <li>
-                                        <a data-pmb-action="edit" href="<?php echo base_url() . "index.php/cliente/editar?id=" . $cliente->id ?>"><i class="zmdi zmdi-edit zmdi-hc-5x"></i></a>
-                                    </li>
-                                </ul>
+                            <div class="col-sm-6">
+                                <h4><i class="zmdi zmdi-assignment-check m-r-5"></i> Hotel Nova Vida</h4>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-            </section>
+        </div>
+</section>
+
+<script type="text/javascript">
+    function adicionarHotelTour()
+    {
+        $.ajax({
+            url: '<?= base_url(); ?>' + 'index.php/cliente/inserirCliente',
+            type: 'POST',
+            data: $("#formCliente").serialize(),
+            success: function (msg) {
+                swal("Cliente inserido com sucesso!", "", "success");
+                $("#nome, #email, #telefone, #nacionalidade, #passaporte, #observacao, #data_contato").val('');
+            }
+        });
+        return false;
+    }
+</script>
