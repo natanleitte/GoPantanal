275b5c945b57864d93d5ec7979754c5359969381
Jorge Luiz Gomes da Silva <jorge.silva.msbr@gmail.com>
2016-05-27 00:21:06 -0400

    Ajuste no calendario da pagina inicial para exibir tardes agendas.

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -225,71 +225,7 @@ $(document).ready(function () {
         });
     }
 
-    /*
-     * Calendar Widget
-     */
-    if ($('#calendar-widget')[0]) {
-        (function () {
-            $('#calendar-widget').fullCalendar({
-                contentHeight: 'auto',
-                theme: true,
-                header: {
-                    right: '',
-                    center: 'prev, title, next',
-                    left: ''
-                },
-                defaultDate: '2014-06-12',
-                editable: true,
-                events: [
-                    {
-                        title: 'All Day',
-                        start: '2014-06-01',
-                        className: 'bgm-cyan'
-                    },
-                    {
-                        title: 'Long Event',
-                        start: '2014-06-07',
-                        end: '2014-06-10',
-                        className: 'bgm-orange'
-                    },
-                    {
-                        id: 999,
-                        title: 'Repeat',
-                        start: '2014-06-09',
-                        className: 'bgm-lightgreen'
-                    },
-                    {
-                        id: 999,
-                        title: 'Repeat',
-                        start: '2014-06-16',
-                        className: 'bgm-lightblue'
-                    },
-                    {
-                        title: 'Meet',
-                        start: '2014-06-12',
-                        end: '2014-06-12',
-                        className: 'bgm-green'
-                    },
-                    {
-                        title: 'Lunch',
-                        start: '2014-06-12',
-                        className: 'bgm-cyan'
-                    },
-                    {
-                        title: 'Birthday',
-                        start: '2014-06-13',
-                        className: 'bgm-amber'
-                    },
-                    {
-                        title: 'Google',
-                        url: 'http://google.com/',
-                        start: '2014-06-28',
-                        className: 'bgm-amber'
-                    }
-                ]
-            });
-        })();
-    }
+    
 
     /*
      * Weather Widget
