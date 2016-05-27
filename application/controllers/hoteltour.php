2d7464dce427f3a2d16f55c663d1f01dd60950cc
natanleitte <natanleitte@gmail.com>
2016-05-27 02:56:13 -0300

    renomeando arquivos hoteltour

--- application/controllers/hoteltour.php
+++ application/controllers/hoteltour.php
@@ -8,10 +8,29 @@ class HotelTour extends CI_Controller {
         parent::__construct();
         $this->load->helper('url');
         $this->load->helper('form');
-
+        $this->load->model('TarefaModel');
+        $this->load->model('EmailModel');
+        $this->load->model('ClienteModel');
+        $this->load->model('UsuarioModel');
+        $this->load->model('HotelModel');
         $this->load->model('HotelTourModel');
         $this->UsuarioModel->estaLogado();
         $this->DataUtils = new DataUtils();
+    }
+
+    public function index() {
+        $this->configuracoesBasicasParaCarregarPagina();
+        $data['tarefas'] = $this->TarefaModel->getTarefas();
+        $data['emails'] = $this->EmailModel->obterTodos();
+        $data['qtdDeEmailsNaoLidos'] = $this->EmailModel->obterQuantidadeDeEmailsNaoLidos();
+        $data['ultimosCincoEmails'] = $this->EmailModel->obterOsUltimosCincoEmails();
+        $data['cliente'] = $this->ClienteModel->getCliente($this->input->get('id', TRUE));
+        $data['hoteis'] = $this->HotelModel->getHoteis();
+        $data['tour'] = $this->input->get('id', TRUE);
+
+        $this->load->view('header', $data);
+        $this->load->view('hoteltour/index', $data);
+        $this->load->view('footer');
     }
 
     public function inserirHotelTour() {
@@ -21,10 +40,18 @@ class HotelTour extends CI_Controller {
         $data['data_ini'] = $this->DataUtils->converteDataParaFormatoDateDoMySql($this->input->post('data_ini'));
         $data['data_fim'] = $this->DataUtils->converteDataParaFormatoDateDoMySql($this->input->post('data_fim'));
         $data['id_tour'] = $this->input->post('tour');
-        
+
         echo $data['id_hotel'];
 
         $this->HotelTourModel->setHotelTour($data);
+    }
+
+    private function configuracoesBasicasParaCarregarPagina() {
+        $this->data['tarefas'] = $this->TarefaModel->getTarefas();
+        $this->data['emails'] = $this->EmailModel->obterTodos();
+        $this->data['qtdDeEmailsNaoLidos'] = $this->EmailModel->obterQuantidadeDeEmailsNaoLidos();
+        $this->data['clientes'] = $this->ClienteModel->getClientesPorDataDesc();
+        $this->data['ultimosCincoEmails'] = $this->EmailModel->obterOsUltimosCincoEmails();
     }
 
 }
