bd6ef545d165f5619d672b1880e413bae486f388
natanleitte <natanleitte@gmail.com>
2016-09-27 01:17:16 -0300

    alteração nos demais orçamentos

--- application/views/cliente/orcamentos/fairtradeII.php
+++ application/views/cliente/orcamentos/fairtradeII.php
@@ -1,21 +1,13 @@
 <div class="card card-orcamento fairtradeII">
-    <div class="card-header ch-alt text-center">
-        <img class="i-logo" src="<?php echo base_url(); ?>assets/img/logo-pequena.png" alt="">
-    </div>
+
+    <?php include "cabecalho.php"; ?>
+
     <div class="card-body card-padding">
         <div class="row">
             <div class="text-center">
                 <h3 class="c-green">Fairtradetour/Amolar:  6 Tage/5 Nächte</h3>
             </div>
         </div>
-
-        <?php include "informacoesDoCliente.php"; ?>
-
-        <div class="clearfix"></div>
-
-        <?php include "valoresDoOrcamento.php"; ?>
-
-        <div class="clearfix"></div>
 
         <div class="p-25">
             <h4 class="c-green f-400">1.Tag</h4>
