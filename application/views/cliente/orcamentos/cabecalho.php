0ce81b80ee5aab4d158aa5ed4176532e1950d58c
natanleitte <natanleitte@gmail.com>
2016-09-27 01:05:39 -0300

    alteração no cabeçalho do orçamento

--- application/views/cliente/orcamentos/cabecalho.php
+++ application/views/cliente/orcamentos/cabecalho.php
@@ -1,3 +1,11 @@
 <div class="card-header ch-alt text-center">
-        <img class="i-logo" src="<?php echo base_url(); ?>assets/img/logo-grande.png" alt="">
+    <img class="i-logo" src="<?php echo base_url(); ?>assets/img/logo-grande.png" alt="">
 </div>
+
+<?php include "informacoesDoCliente.php"; ?>
+
+<div class="clearfix"></div>
+
+<?php include "valoresDoOrcamento.php"; ?>
+
+<div class="clearfix"></div>
