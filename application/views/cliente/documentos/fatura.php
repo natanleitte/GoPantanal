92816683e5dcd3be38a6490f85b87d1efb1e7624
natanleitte <natanleitte@gmail.com>
2016-12-14 18:55:02 -0200

    finalizando a fatura

--- application/views/cliente/documentos/fatura.php
+++ application/views/cliente/documentos/fatura.php
@@ -10,94 +10,57 @@
         </div>
 
 
-        <div class="span 6">
-             <div class="form form-inline">
-                 <h4>Reisedatum:
-                <input id="valor" type='text' placeholder="Entrada" />
+        <div class="col-xs-6">
+            <div class="form form-inline">
+                <h4>Reisedatum:
+                    <input id="valor" type='text' class="form-control" placeholder="Reisedatum" />
+            </div>
+            <div class="form form-inline">
+                <h4>Tour:
+                    <input id="valor" type='text' class="form-control" placeholder="Tour" />
+            </div>
+            <div class="form form-inline">
+                <h4>Anmerkungen:
+                    <input id="valor" type='text' class="form-control" placeholder="Anmerkungen" />
             </div>
         </div>
 
-        <div class="p-25">
-
-
-            <br/>
-
-            <h4 class="c-green f-400">2.Tag</h4>
-            <p>Morgens und Nachmittags, Safariaktivitäten.</p>
-
-            <br/>
-
-            <h4 class="c-green f-400">3.Tag</h4>
-            <p>Morgens, nochmals Safariaktivität, nach dem Mittagessen check-out und Transfer mit shuttlebus nach Bonito.
-                Ankunft um ca. 18:00. Unterkunft im privaten Doppelzimmer in schöner Pousada (Galeria Arte), mit Pool, angenehmer 
-                Anlage und super Frühstück.</p>
-
-            <br/>
-
-            <h4 class="c-green f-400">4.Tag</h4>
-            <p>Morgens, bester Schnorchelausflug Bonito’s Rio da Prata (inkl. Mittagessen), nachmittags, Besuch der Rot-Ara-
-                Kolonie, mit Dutzenden von freilebenden roten Ara-Papagaien.</p>
-
-            <br/>
-
-            <h4 class="c-green f-400">5.Tag</h4>
-            <p>Morgens, Besuch der Grotte mit blauem See. Um 12:30 Abfahrt mit shuttlebus nach Campo Grande City oder
-                Flughafen. Ankunft um ca 17:00.</p>
+        <div class="col-xs-6">
+            <div class="form form-inline">
+                <h4>Daten zur Banküberweisung:</h4>
+                Postfinance<br>
+                Kontonummer: 25-546648-6<br>
+                IBAN: Ch5609000000255466486<br>
+                BIC/BLZ POFICHBEXXX<br>
+                Martin Ann<br>
+                Riedhofstrasse 366<br>
+                8049 Zürich<br>
+            </div>
         </div>
 
-        <div class="clearfix"></div>
-        <table class="table i-table m-t-25 m-b-25">
-            <thead class="text-uppercase">
-            <th class="c-gray"></th>
-            </thead>
-            <tbody>
-            <thead>
-                <tr>
-                    <td>
-                        <h5 class="text-uppercase f-400">Inbergriffen</h5>
-                        <p class="text-muted">Alle Transfers, Übernachtungen im Pantanal mit Vollpension, in Bonito mit Frühstück, alle Safariaktivitäten
-                            im Pantanal inkl.englischspr. Guide, alle Ausflüge und Transfers in Bonito, alle Taxen.</p>
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        <h5 class="text-uppercase f-400">Nicht inbegriffen</h5>
-                        <p class="text-muted">Mittagessen am 1. und 5. Tag, Nachtessen in Bonito, Getränke und pers, Ausgaben.</p>
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        <h5 class="text-uppercase f-400">Safariaktivitäten im Pantanal sind</h5>
-                        <p class="text-muted">Jeepsafari, Dschungelwanderung, Bootsafari, Nachtexpedition, Kanufahren, Piranha
-                            Angeln. Die Reihenfolge dieser Aktivitäten wird von der Lodge organisiert.</p>
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        <h5 class="text-uppercase f-400">Anmerkungen</h5>
-                        <p class="text-muted">Bei dieser Tour sind sie zusammen mit Pantanalbesuchern von anderen Agenturen unterwegs, besitzen
-                            aber ihre Privatsphäre in den Unerkünften. Es ist alles bestens organisert für ein eindrückliches und sorgloses 
-                            Pantanalerlebnis. Die Safariaktivitäten finden in  Gruppen bis ca. 10 Personen statt.</p>
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        <h5 class="text-uppercase f-400">Für Übernachtungen in Campo Grande:</h5>
-                        <p class="text-muted">Wir besitzen ein schönes Haus mit 2 Gästezimmern, Pool, Varanda, Gratis-WiFi, etc.  
-                            Übernachtung mit Nachtessen und Frühstück + Pickup Flughafen oder Busstation:  <b>Preis 35.- Euro/Person</b></p>
-                    </td>
-                </tr>
-            </thead> 
-            </tbody>
-        </table>
-        <div class="row m-b-25">
-            <p class="text-center">
-                <b>Unbedingt mitnehmen:</b> Moskitorepellent, Sonnenschutz, Turnschuhe oder leichte Wanderschuhe, leichte Kleidung (1
-                Garnitur langärmelig bzw. Hosig), leichter Regenschutz, Fotomaterial.
-                <br/>
-                <b>Ihre Buchung wird bestätigt durch Anzahlung von 30% des Gesamtbetrags.
-                    Die restlichen 70 % können bis max. 7 Tage vor Reisebeginn überwiesen werden.</b>
-            </p>
+        <div class="col-xs-12">
+            <h4>Rücktrittsbestimmungen:</h4>
+            <br>
+            <br>
+            Bei Rücktrittserklärung ihrer gebuchten Tour, informieren sie uns bitte umgehend per e-mail an
+            folgende Adressen: <strong>info@gopantanal.com</strong> oder <strong>pantanal@gmx.net.</strong>
+            Das Eingangsdatum ihrer Rücktrittserklärung ist ausschlaggebend für die Stornierungsgebühren.
+            <div class="col-xs-6">
+                <br>
+                bis 60 Tage vor Reisebeginn<br>
+                59-30 Tage vor Reisebeginn<br>
+                29-7 Tage vor Reisebeginn<br>
+                6-0 Tage vor Reisebeginn
+                <br>
+            </div>
+            <div class="col-xs-6">
+                <br>
+                50% der Anzahlung<br>
+                75% der Anzahlung<br>
+                100% der Anzahlung<br>
+                75% des Gesamtbetrages
+                <br>
+            </div>
         </div>
     </div>
     <?php include "rodape.php"; ?>
